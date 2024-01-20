a90920007b6c39bef06c152414afb3985d12f2e7
ilD1k <151182252+ilD1k@users.noreply.github.com>
2024-01-21 00:38:44 +0500

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,77 @@
+import telebot
+from config import token
+bot = telebot.TeleBot(token)
+
+
+
+@bot.message_handler(commands=["start"])
+def start(message):
+    name = message.chat.first_name
+    bot.send_message(message.chat.id, f'Привет! {name}.\n')
+    bot.send_message(message.chat.id, "это новый бот квестик by ilD1k")
+    bot.send_message(message.chat.id, "Твой первый вопрос"
+                                      " Ты хочешь посуетить но как??"
+                                      " 1) дотка"
+                                      " 2) школа"
+                                      " 3) гулять")
+
+    bot.register_next_step_handler(message, user_answer)
+
+
+@bot.message_handler(commands=["help"])
+def help(message):
+    bot.send_message(message.chat.id, "Все сломал?? Вызывай старшего")
+    bot.send_message(message.chat.id, 'Свяжись с [создателем](https://t.me/ildics)',
+                     parse_mode='Markdown', )
+
+def user_answer(message):
+    if message.text == "1":
+        bot.send_message(message.chat.id, "ты выйграл катку"
+                                          " У тебя выбор"
+                                          " 1) на седня хватит"
+                                          " 2) регну еще ")
+        bot.register_next_step_handler(message, vibor_dota)
+
+    elif message.text == "2":
+        bot.send_message(message.chat.id, "ты че???"
+                                          " Квестик - провален")
+        bot.register_next_step_handler(message, start)
+
+
+    elif message.text == "3":
+        bot.send_message(message.chat.id, "Ты оч успешно пошел гулять"
+                                          " Выбор "
+                                          " 1) Ну че домой пойду доту регну"
+                                          " 2) Пойду к моему bro")
+        bot.register_next_step_handler(message, vibor)
+
+
+def vibor_dota(message):
+    if message.text == "1":
+        bot.send_message(message.chat.id, "Квест пройден - ПОБЕДА")
+        bot.register_next_step_handler(message, start)
+
+    elif message.text == "2":
+        bot.send_message(message.chat.id, "Ты слил 100 ммр "
+                                          " Квест пройден - ПОРАЖЕНИЕ")
+        bot.register_next_step_handler(message, start)
+
+    else:
+        bot.send_message(message.chat.id, "ты чет не то написал")
+
+
+def vibor(message):
+    if message.text == "1":
+        bot.send_message(message.chat.id ("Ты пришел домой и регнул доту"
+                                          " Квест пройден - ПОБЕДА"))
+        bot.register_next_step_handler(message, start)
+
+    if message.text == "2":
+        bot.send_message(message.chat.id, "Ты успешно сходил к bro"
+                                          " Квест пройден - ПОБЕДА")
+    else:
+        bot.send_message(message.chat.id, "ты чет не то написал")
+
+
+bot.polling()
+
